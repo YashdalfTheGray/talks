c9a380ed728a77d5da89dc1f4a7a1a57180a3518
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-06 22:55:03 -0700

    finished out the presentation

--- learning/presentation/index.js
+++ learning/presentation/index.js
@@ -3,13 +3,10 @@ import preloader from 'spectacle/lib/utils/preloader';
 import createTheme from 'spectacle/lib/themes/default';
 
 import {
-    BlockQuote,
-    Cite,
     Deck,
     Heading,
-    ListItem,
-    List,
-    Quote,
+    Image,
+    Link,
     Slide,
     Text
 } from 'spectacle';
@@ -17,7 +14,11 @@ import {
 require('normalize.css');
 require('spectacle/lib/themes/default/index.css');
 
-const images = {};
+const images = {
+    commit: require('../assets/commit.png'),
+    shadow: require('../assets/shadow.jpg'),
+    trello: require('../assets/trello.svg')
+};
 
 preloader(images);
 
@@ -34,13 +35,97 @@ const theme = createTheme({
 export default class Presentation extends React.Component {
     render() {
         return (
-            <Deck transition={['zoom', 'slide']} transitionDuration={500} theme={theme}>
-                <Slide transition={['zoom']} bgColor="primary">
+            <Deck transition={['fade']} transitionDuration={500} theme={theme}>
+                <Slide transition={['fade']} bgColor="primary">
                     <Heading size={1} fit caps lineHeight={1} textColor="secondary">
                         Managing Your Learning
                     </Heading>
-                    <Text margin="30px 0 0" textColor="tertiary" size={4}>
+                    <Text margin="30px" textColor="tertiary" textSize={48}>
                         Yash Kulshrestha
+                    </Text>
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Text textColor="tertiary" textSize={32} margin="10px">
+                        Standard issue codemonkey
+                    </Text>
+                    <Text textColor="tertiary" textSize={32} margin="10px">
+                        Gamer
+                    </Text>
+                    <Text textColor="tertiary" textSize={32} margin="10px">
+                        Buckeye
+                    </Text>
+                    <Text textColor="tertiary" textSize={32} margin="10px">
+                        Petrolhead
+                    </Text>
+                    <Text textColor="tertiary" textSize={32} margin="10px">
+                        Dude
+                    </Text>
+                    <Text textColor="tertiary" textSize={32} margin="10px">
+                        Emoji enthusiast
+                    </Text>
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Image src={images.commit} width="100%" />
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Text textColor="tertiary" textSize={120}>
+                        🤔
+                    </Text>
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Image src={images.shadow} width="100%" />
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Text textColor="tertiary" textSize={120}>
+                        🕔
+                    </Text>
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Text textColor="tertiary" textSize={48}>
+                        Rome wasn't built in a day.
+                    </Text>
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Image src={images.trello} width="60%" />
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Text textColor="tertiary" textSize={120}>
+                        🥅 🥅 🥅
+                    </Text>
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Text textColor="tertiary" textSize={48}>
+                        Motivation is what gets you started...
+                    </Text>
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Text textColor="tertiary" textSize={48}>
+                        ...habit is what keeps you going.
+                    </Text>
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Link href="https://github.com/yashdalfthegray" textColor="tertiary" textSize={48} margin="24px">
+                        Github
+                    </Link>
+                    <Link href="https://twitter.com/yashdalfthegray" textColor="tertiary" textSize={48} margin="24px">
+                        Twitter
+                    </Link>
+                    <Link href="https://yashkulshrestha.carrd.co" textColor="tertiary" textSize={48} margin="24px">
+                        Website
+                    </Link>
+                    <Text textColor="secondary" textSize={48} margin="24px ">
+                        ~~
+                    </Text>
+                    <Link href="https://github.com/yashdalfthegray" textColor="tertiary" textSize={48} margin="24px">
+                        Presentation Notes
+                    </Link>
+                </Slide>
+                <Slide transition={['fade']} bgColor="primary">
+                    <Heading size={1} caps lineHeight={1} textColor="secondary">
+                        Thank you
+                    </Heading>
+                    <Text margin="48px 0 0" textColor="tertiary" textSize={24}>
+                        Any feedback is welcome!
                     </Text>
                 </Slide>
             </Deck>
