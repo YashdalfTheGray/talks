d0e6c905e4618ad913c6ab0ab8976c8613f246ce
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-03-02 01:40:11 -0800

    added some slides to the presentation

--- loader/presentation/index.js
+++ loader/presentation/index.js
@@ -1,15 +1,12 @@
-// Import React
 import React from 'react';
-
-// Import Spectacle Core tags
 import {
     Deck,
     Heading,
+    Link,
     Slide,
     Text
 } from 'spectacle';
-
-// Import theme
+import CodeSlide from 'spectacle-code-slide';
 import createTheme from 'spectacle/lib/themes/default';
 
 // Require CSS
@@ -39,6 +36,29 @@ export default class Presentation extends React.Component {
                     <Text margin="30px" textColor="tertiary" textSize={48}>
                         Yash Kulshrestha
                     </Text>
+                </Slide>
+                <CodeSlide
+                    bgColor="#fff"
+                    color="#555"
+                    lang="js"
+                    code={require('raw-loader!../assets/config.txt')}
+                    ranges={[{ loc: [2, 19] }]} />
+                <Slide transition={['fade']} bgColor="primary">
+                    <Link href="https://github.com/yashdalfthegray" target="_blank" textColor="tertiary" textSize={48} margin="24px">
+                        Github
+                    </Link>
+                    <Link href="https://twitter.com/yashdalfthegray" target="_blank" textColor="tertiary" textSize={48} margin="24px">
+                        Twitter
+                    </Link>
+                    <Link href="https://yashkulshrestha.carrd.co" target="_blank" textColor="tertiary" textSize={48} margin="24px">
+                        Website
+                    </Link>
+                    <Text textColor="secondary" textSize={48} margin="24px ">
+                        ~~
+                    </Text>
+                    <Link href="https://yashdalfthegray.github.io/talks/loader/notes" target="_blank" textColor="tertiary" textSize={48} margin="24px">
+                        Presentation Notes
+                    </Link>
                 </Slide>
             </Deck>
         );
